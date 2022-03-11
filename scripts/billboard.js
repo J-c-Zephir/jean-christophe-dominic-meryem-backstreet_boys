c9b6c6886b8527b2c129b33268579aacb7c280dc
J-c-Zephir <jczephir1@gmail.com>
2022-03-10 23:21:12 -0500

    Swiper Terminé

--- scripts/billboard.js
+++ scripts/billboard.js
@@ -1,21 +1,20 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+
+  }
+});
