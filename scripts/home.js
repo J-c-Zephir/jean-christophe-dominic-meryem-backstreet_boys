4d95cb0afd633c3c4a708eeff32aecd7bf7b11bc
Mery <Meryberbiche@gmail.com>
2022-03-30 22:07:05 -0400

    tp05 fini?

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,11 @@
+gsap.registerPlugin(ScrollTrigger);
+
 let swiper = new Swiper(".swiper", {
     direction:'horizontal',
     loop: false,
 
     pagination:{
-         el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
     
     navigation:{
@@ -12,6 +14,42 @@ let swiper = new Swiper(".swiper", {
     },
 
     scrollbar: {
-         el: '.swiper-scrollbar'
+        el: '.swiper-scrollbar'
     }
 });
+
+
+let sections = document.querySelectorAll('section')
+
+sections.forEach(function(section) {
+
+    gsap.timeline({
+        scrollTrigger: {
+          trigger: section,
+          toggleActions: 'restart complete reverse reset',
+          }
+        })
+        .from(section, {opacity:0})
+        .to(section, {opacity: 100})
+
+    let images = section.querySelectorAll('section img');
+      images.forEach(function(img) {
+      gsap.timeline({
+        scrollTrigger: {
+            trigger: img,
+            toggleActions: 'restart complete reverse reset',
+            }
+      })
+      .from(img, {scale:0})
+      .to(img, {scale: 1})
+      });
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: 'video',
+          toggleActions: 'restart complete reverse reset',
+          }
+        })
+        .from('video', {scale:0})
+        .to('video', {scale: 1})
+  });
