8472e40c8003b492e2611deb14989820821952fd
CKYKA513 <90738854+CKYKA513@users.noreply.github.com>
2022-03-30 22:09:56 -0400

    fhas

--- scripts/discography.js
+++ scripts/discography.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 let swiper = new Swiper(".swiper", {
     navigation: {
         nextEl: '.swiper-button-next',
@@ -12,3 +14,24 @@ let swiper = new Swiper(".swiper", {
         runCallbacks = true
     },
 });
+
+// gsap.to('.album1', {
+//     markers: true,
+//     x: '100%',
+//     duration: 4,
+// });
+
+let tp5 = gsap.timeline({
+    duration: 4,
+    scrallTrigger: {
+        markers: true,
+        start: 'center',
+        end: 'center',
+        toggleActions: 'restart complete reverse reset',
+    }
+})
+.from('section', { x: '0%' })
+.to('section', { x: '100%' })
+.to('section', { x: '0%' })
+.from('section', { x: '100%' })
+    
