0f8ec7ec9cc7d711feacef8ee4686754a500b09f
J-c-Zephir <90851637+J-c-Zephir@users.noreply.github.com>
2022-04-11 15:12:10 -0400

    euh
    balls?

--- scripts/billboard.js
+++ scripts/billboard.js
@@ -20,7 +20,7 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
-
+// Toutes les animations on scroll
 let section = document.querySelectorAll('section')
 
 section.forEach(function (section) {
@@ -57,3 +57,43 @@ section.forEach(function (section) {
     .from('video', { scale: 0 })
     .to('video', { scale: 1 })
 });
+
+// Callback
+const body = document.querySelector('body');
+let isScrolling;
+
+// ***** Instruction 2 ***** //
+window.addEventListener('scroll', function() {
+	window.clearTimeout( isScrolling );
+  body.classList.add("is-scrolling");
+  
+  // ***** Instruction 3 ***** //
+	isScrolling = setTimeout(function() {
+		console.log( 'Scrolling has stopped.' );
+    body.classList.remove("is-scrolling");
+	}, 250);
+});
+
+// ***** Instruction 1 ***** //
+let anim = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.level',
+    
+    onUpdate: (e) => {
+      
+      if(e.progress){
+        // ***** Instruction 4 ***** //
+         if(e.direction==-1){
+          body.classList.add("direction-up");
+          body.classList.remove("direction-down");
+        }else{
+          // ***** Instruction 5 ***** //
+          body.classList.add("direction-down");
+          body.classList.remove("direction-up");
+        }
+      }
+    }
+  
+  }
+});
+
